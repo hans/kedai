8c0c3f0afc95b666fa7f186556a0a740def3f190
Hans Engel <engel@engel.uk.to>
2012-04-01 11:30:19 -0700

    Basic project metadata

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject kedai "1.0.0-SNAPSHOT"
   :description "In-memory key-value store"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [io.netty/netty "3.3.1.Final"]])
+                 [io.netty/netty "3.3.1.Final"]]
+  :main kedai.server
+  :run-aliases {:server kedai.server}
+  :warn-on-reflection true
+  :source-paths ["src"]
+  :test-paths ["test"])
